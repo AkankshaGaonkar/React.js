932b9119a6114f39aa6a43b0f68824b44c6ff681
AkankshaGaonkar <ashugaonkar2000@gmail.com>
2022-08-09 18:41:22 +0530

    add

---
+++ ImportExport/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import add, { multiply, subtract, divide } from "./app.jsx";
+//import * as Calculator from "./calculator";
+
+ReactDOM.render(
+  <ul>
+    <li>{add(1, 2)}</li>
+    <li>{multiply(2, 3)}</li>
+    <li>{subtract(7, 2)}</li>
+    <li>{divide(5, 2)}</li>
+  </ul>,
+  document.getElementById("root")
+);
+
+//<li>{Calculator.add(1,2)}</li>
